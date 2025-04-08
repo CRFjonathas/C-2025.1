29a8e5b4f950896dec8dd75f937c9455973ac733
CRFjonathas <jonathas.github@gmail.com>
2025-04-08 19:04:45 +0000

    criação de um programa simples com if e else

--- trunfo.c
+++ trunfo.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste Jonathas Nicacio Silva
+#include <stdio.h> // serve para incluir a biblioteca padrão de entrada e saida de dados
+#include <string.h> // serve para incluir a biblioteca padrão de manipulação de strings
 
 int main(){
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // VARIAVEIS:
 
-    char estado_1[2],codigo_1, cidade_1[50];
-    char estado_2[2],codigo_2, cidade_2[50];
-    long int populacao_1, populacao_2;   
-    float area_1, area_2, pib_1, pib_2;
-    int  pontos_turisticos_1, pontos_turisticos_2;
+    char estado_1,estado_2; // string
+    char codigo_2[5], codigo_1[5]; // string
+    char cidade_1[50], cidade_2[50]; // string
+    unsigned long int populacao_1, populacao_2;   // serve para armazenar um numero muito grande
+    float area_1, area_2, pib_1, pib_2; // numeros decimais
+    int  pontos_turisticos_1, pontos_turisticos_2; // numero inteiro
+
+    // IMPRIMINDO AS REGRAS DO JOGO: 
 
     printf("REGRAS:\n");
     printf("\n- Cada país será dividido em 8 estados (identificadas pelas letras A a H).\n");
@@ -24,51 +21,46 @@ int main(){
     printf("\nEXEMPLO: A01; A02; B01; B02...\n");
     printf(" \n");    
 
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // CADASTRO DA CARTA 1:
 
-    printf("\nCarta 1: \n");
+    printf("\nDigite os dados para a Carta 1: \n");
     printf(" \n");
-    printf("\nEstado: ");
-    scanf("%s", &estado_1);
-    printf("Código: %s0", estado_1);
-    scanf("%s", &codigo_1);
-    
-    //limpar buffer de entrada
-    getchar(); // isso consome o '\n' que sobra apos o scanf
+    printf("\nEstado (A a H): ");
+    scanf(" %c", &estado_1); // "%c" usado para cadastrar ou imprimir unico caractere
+    printf("Código da carta (ex: A01): ");
+    scanf("%s", codigo_1); // "%s" usado para uma cadeia de caracteres (string)
 
     printf("Nome da cidade: ");
-    fgets(cidade_1, 50, stdin);
-    cidade_1[strcspn(cidade_1, "\n")] = 0;
+
+    getchar(); // Remove o caractere de nova linha (\n) que ficou na entrada após uma leitura anterior.
+    fgets(cidade_1, sizeof(cidade_1), stdin); // Lê uma linha de texto (incluindo espaços)
+    cidade_1[strcspn(cidade_1, "\n")] = 0; // Remove o caractere de nova linha (\n) do final da string lida
     
     printf("População: ");
-    scanf("%ld", &populacao_1);
+    scanf("%lu", &populacao_1); // "%lu" usado para "unsigned long int"
     printf("Área: ");
-    scanf("%f", &area_1);
+    scanf("%f", &area_1); //"%f" usado para numeros flutuantes
     printf("PIB: ");
     scanf("%f", &pib_1);
     printf("Número de Pontos Turisticos: ");
-    scanf("%d",&pontos_turisticos_1);
+    scanf("%d",&pontos_turisticos_1); // "%d" usado para numeros inteiros
 
+    // CADASTRO DA CARTA 2:
 
-    // DADOS DA CARTA 2:
-
-    printf("\nCarta 2: \n");
+    printf("\nDigite os dados para a Carta 2: \n");
     printf(" \n");
-    printf("\nEstado: ");
-    scanf("%s", &estado_2);
-    printf("Código: %s0", estado_2);
-    scanf("%s", &codigo_2);
-    
-    getchar();
+    printf("\nEstado (A a H): ");
+    scanf(" %c", &estado_2);
+    printf("Código da carta (ex: B02): ");
+    scanf("%s", codigo_2);
 
     printf("Nome da cidade: ");
+    getchar();
     fgets(cidade_2, 50, stdin);
     cidade_2[strcspn(cidade_2, "\n")] = 0;
     
     printf("População: ");
-    scanf("%ld", &populacao_2);
+    scanf("%lu", &populacao_2);
     printf("Área: ");
     scanf("%f", &area_2);
     printf("PIB: ");
@@ -76,40 +68,59 @@ int main(){
     printf("Número de Pontos Turisticos: ");
     scanf("%d",&pontos_turisticos_2);
 
-    //ÁREA DE CALCULO:
+    // CALCULO DA DENSIDADE POPULACIONAL E PIB PER CAPITA:
 
-    float pib_per_capita_1 =  (pib_1 * 1000000000) / populacao_1; 
-    float densidade_pop_1 = populacao_1 / area_1;
-    float pib_per_capita_2 =  (pib_2 * 1000000000) / populacao_2;
-    float densidade_pop_2 = populacao_2 / area_2;
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    float densidade_pop_1 = (float)populacao_1 / area_1; // converte a população para float
+    float pib_per_capita_1 =  pib_1 * 1000000000 / populacao_1; // coverte o PIB para BILHÃO
+    float densidade_pop_2 = (float)populacao_2 / area_2;    
+    float pib_per_capita_2 =  pib_2 * 1000000000 / populacao_2; 
 
-    printf(" \n");
+    // CALCULO DO SUPER PODER DA CARTA:
+
+    float super_poder_1 = populacao_1 + area_1 + (pib_1 * 1000000000) + pontos_turisticos_1 + pib_per_capita_1 +(1 / densidade_pop_1);
+    float super_poder_2 = populacao_2 + area_2 + (pib_2 * 1000000000) + pontos_turisticos_2 + pib_per_capita_2 +(1 / densidade_pop_2);
+
+    //EXIBIÇÃO DOS DADOS DA CARTA 1:
+
     printf("\nCARTA 1:\n");
-    printf("\nESTADO: %s\n", estado_1);
-    printf("CODIGO DA CIDADE: %s0%d\n",estado_1, codigo_1 );
+    printf("\nESTADO: %c\n", estado_1);
+    printf("CÓDIGO: %s\n", codigo_1 );
     printf("CIDADE: %s\n",cidade_1 );
-    printf("POPULAÇÃO: %ld habitates\n",populacao_1 );
+    printf("POPULAÇÃO: %lu habitantes\n",populacao_1 );
     printf("ÁREA: %.2f km²\n",area_1 );
     printf("PIB: R$ %.2f BILHÕES\n",pib_1 );
     printf("NÚMERO DE PONTOS TURISTICOS: %d\n",pontos_turisticos_1 );
     printf("Densidade Populacional: %.2f hab/Km².\n", densidade_pop_1);
     printf("PIB per Capita: R$ %.2f\n", pib_per_capita_1);
+    printf("SUPER PODER: %.2f\n", super_poder_1);
 
-    printf(" \n");
+    //EXIBIÇÃO DOS DADOS DA CARTA 2:
+
     printf("\nCARTA 2:\n");
-    printf("\nESTADO: %s\n", estado_2);
-    printf("CODIGO DA CIDADE: %s0%d\n",estado_2, codigo_2 );
+    printf("\nESTADO: %c\n", estado_2);
+    printf("CÓDIGO: %s\n", codigo_2 );
     printf("CIDADE: %s\n",cidade_2 );
-    printf("POPULAÇÃO: %ld habitates\n",populacao_2 );
+    printf("POPULAÇÃO: %lu habitantes\n",populacao_2 );
     printf("ÁREA: %.2f km²\n",area_2 );
     printf("PIB: R$ %.2f BILHÕES\n",pib_2 );
     printf("PONTOS TURISTICOS: %d\n",pontos_turisticos_2 );
     printf("Densidade Populacional: %.2f hab/Km².\n", densidade_pop_2);
     printf("PIB per Capita: R$ %.2f\n", pib_per_capita_2);
+    printf("SUPER PODER: %.2f\n", super_poder_2);
+
+    //EXIBIÇAO DA COMPARAÇAO DAS CARTAS:
+
+    printf("\nCOMPARAÇÃO DE CARTAS:\n");
+    printf("população: Carta %d venceu (%d)\n", (populacao_1 > populacao_2) ? 1 : 2, (populacao_1 > populacao_2) ? 1 : 0);
+    printf("Área: Carta %d venceu (%d)\n", (area_1 > area_2) ? 1 : 2, (area_1 > area_2) ? 1 : 0);
+    printf("PIB: Carta %d venceu (%d)\n", (pib_1 > pib_2) ? 1 : 2, (pib_1 > pib_2) ? 1 : 0);
+    printf("Pontos Turisticos: Carta %d venceu (%d)\n", (pontos_turisticos_1 > pontos_turisticos_2) ? 1 : 2, (pontos_turisticos_1 > pontos_turisticos_2));
+    printf("Densidade Populacional: Carta %d venceu (%d)\n", (densidade_pop_1 < densidade_pop_2) ? 1 : 2, (densidade_pop_1 < densidade_pop_2));
+    printf("PIB per Capita: Carta %d venceu (%d)\n", (pib_per_capita_1 > pontos_turisticos_2) ? 1 : 2, (pib_per_capita_1 > pib_per_capita_2));
+    printf("Super Poder: Carta %d venceu (%d)\n", (super_poder_1 < super_poder_2) ? 1 : 2, (super_poder_1 > super_poder_2));
+
+    /* O primeiro %d exibe 1 se X > Y, ou 2 caso contrário (indica a carta vencedora).
+       O segundo %d exibe 1 se X > Y, ou 0 caso contrário (indica se a vitória é válida ou não).*/
 
     return 0;
 }
