b698fb73c1e6c5e0800ea6dec69bd8e01f82688f
CRFjonathas <jonathas.github@gmail.com>
2025-05-06 12:39:51 +0000

    prgramas de estrutura de decisao aninhadas e encadeadas

--- estrut_decisão_aninhadas.c
+++ estrut_decisão_aninhadas.c
@@ -2,6 +2,33 @@
 
 int main(){
 
-    
+    // o usuario estar entre 18 e 65 anos
+    //renda menor que 3000
+    //numero de dependentes maior que 2
 
+    int idade, dependentes;
+    float renda;
+
+    printf("Sua idade: ");
+    scanf("%d", &idade);
+    printf("Renda mensal: ");
+    scanf("%f", &renda);
+    printf("Quantos dependentes você possui: ");
+    scanf("%d", &dependentes);
+
+    if (idade >= 18 && idade <= 65){
+        if(renda < 3000){
+            if(dependentes > 2){
+                printf("Você tem direito a beeficio!\n");
+            } else{
+                printf("Você não atende ao criterio de dependentes!\n");
+            }
+
+        } else {
+            printf("Você não atende ao criterio renda!\n");
+        }
+    } else{
+        printf("Você não atende ao criterio idade!\n");
+    }
+    
 }
